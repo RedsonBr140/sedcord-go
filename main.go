a24c8d944dbc5948200661f4795377d4e6604e8c
Redson <redson@riseup.net>
2022-07-22 08:08:28 -0300

    feat: GameStatus

--- main.go
+++ main.go
@@ -30,8 +30,9 @@ func main(){
     return
   }
 
+  dg.AddHandler(ready)
   dg.AddHandler(messageCreate)
-  
+
   // Open a websocket connection to Discord
   if err := dg.Open(); err != nil {
     fmt.Println("Error oppening connection:", err)
@@ -89,4 +90,10 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate){
     }
     s.ChannelMessageSendReply(m.ChannelID, text, m.ReferencedMessage.Reference())
   }
+}
+
+func ready(s *discordgo.Session, r *discordgo.Ready){
+  log.Printf("Logged in as: %v#%v", s.State.User.Username, s.State.User.Discriminator)
+  status := fmt.Sprintf("sed at %d servers", len(r.Guilds))
+  s.UpdateGameStatus(0, status)
 }
